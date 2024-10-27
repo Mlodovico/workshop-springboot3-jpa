d78656fb798721bf1427284ec38bf9766121969d
mlodovico <murilo.lodovico@outlook.com>
2024-10-27 19:04:01 -0300

    feature: creating and association table order and entity

---
+++ src/main/java/com/mlodovico/course/entities/Order.java
@@ -0,0 +1,74 @@
+package com.mlodovico.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name="tb_order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name="client_id")
+    private User cliend;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, User cliend) {
+        this.id = id;
+        this.moment = moment;
+        this.cliend = cliend;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getCliend() {
+        return cliend;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if(id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
+}
--- src/main/java/com/mlodovico/course/entities/User.java
+++ src/main/java/com/mlodovico/course/entities/User.java
@@ -3,6 +3,8 @@ package com.mlodovico.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="tb_user")
@@ -17,6 +19,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany
+    @JoinColumn(name="order_id")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -67,6 +73,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
